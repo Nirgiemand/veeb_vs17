c707cfe6ce7e5b395462f732544ab8d513acf446
el Bobo <henri.tamvere@khk.ee>
2017-12-11 09:39:54 +0200

    Mina faili loomine

---
+++ mina.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: el Bobo
+ * Date: 11.12.2017
+ * Time: 9:10
+ *
+    */
+
+$minunimi = "Henri";
+echo "Minu nimi on $minunimi <br />";
+print 'Minu nimi on '.$minunimi.'<br />';
+print("Minu nimi on $minunimi <br />");
+?>
