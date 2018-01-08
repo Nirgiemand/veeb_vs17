c6185dee7b5deab9f6a5bc6f0381d401f65e0246
Nirgiemand <henri.tamvere@khk.ee>
2018-01-08 14:21:38 +0200

    funktsioonid

--- funk.php
+++ funk.php
@@ -8,11 +8,45 @@
 function raamatuVorm () {
     echo '
         <form action="'.$_SERVER['PHP_SELF'].'" method="post">
-        Pealkiri: <input type="text" name="title" <br />
-        Autor: <input type="text" name="author" <br />
-        Trükikoda <input type="text" name="print" <br />
-        Seisund <input type="text" name="status" <br />
-        <input type="submit" value="Salvesta!" >
+            Pealkiri: <input type="text" name="title"> <br />
+            Autor: <input type="text" name="author"> <br />
+            Trükikoda: <input type="text" name="print"> <br />
+            Seisund: <input type="text" name="status"> <br />
+            <input type="submit" value="Salvesta!" >
         </form>
     ';
+}
+
+function salvestaRaamat ($raamat, $failinimi) {
+    if(file_exists($fail) and is_file($fail) and is_writable($fail)
+    ){
+        $fail=fopen($failinimi, 'a') or die ('Probleem faili avamisega');
+        foreach ($raamat as $element) {
+            fwrite($fail, $element."\n");
+        }
+        fwrite($fail, "-----\n");
+        fclose($fail);
+        echo 'Andmed on sisestatud <br />';}
+    else {
+        echo 'Probleem failiga '.$failinimi.'<br />';
+        }
+    };
+
+function loeAndmeid ($failinimi) {
+    if(file_exists($failinimi) and is_file($failinimi) and is_readable($failinimi))  {
+        $fail = fopen($failinimi,'r');
+        echo '<table border="1">';
+            echo '<tr>';
+                echo '<th> Pealkiri </th>';
+                echo '<th> Autor </th>';
+                echo '<th> Trükikoda </th>';
+                echo '<th> Seisund </th>';
+            echo '</tr>';
+            while (! feod($fail)) {
+                echo '<td>';
+                echo fgets($fail);
+                echo '</td>';
+            }
+        echo '</table>';
+    }
 }
