514b2c1b359e003576d6f7ba3efd79b6e6c72299
Nirgiemand <henri.tamvere@khk.ee>
2018-01-15 09:18:36 +0200

    määratud tekst

--- obj/test.php
+++ obj/test.php
@@ -11,7 +11,7 @@ require_once 'tekst.php';
 
 $minuTekst = new tekst();
 // väärtustame sone muutuja
-$minuTekst->sone ='Tere maailm!';
+$minuTekst->maaraTekst('Tere maailm!');
 
 // teostame test-vaade antud objektist
 
