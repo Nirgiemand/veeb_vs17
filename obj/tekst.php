51b2880452e5bf99e73d6618a118cbffe297339a
Nirgiemand <henri.tamvere@khk.ee>
2018-01-15 08:57:22 +0200

    klassi omadused ja muutujad

---
+++ obj/tekst.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri.tamvere
+ * Date: 15.01.2018
+ * Time: 8:51
+ */
+
+class tekst
+{
+    // klassi omadused
+    // klassi muutujad
+    var $sone = '';
+}
