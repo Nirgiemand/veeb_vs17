7f35ff85d426d645d4e5113796fc67d8b4901a45
Nirgiemand <henri.tamvere@khk.ee>
2018-01-15 12:27:28 +0200

    loodud klass tabel ja defineeritud klassi muutujad

---
+++ obj/tabel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri.tamvere
+ * Date: 15.01.2018
+ * Time: 12:24
+ */
+
+class tabel
+{
+    // klassi omadused
+    var $tabeliSisu = array();
+    var $pealkirjad = array();
+    var $veergudeArv;
+}
