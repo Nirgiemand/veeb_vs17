4e5fe9fcfd0a929c12bde0618c330e6fe315d370
Nirgiemand <henri.tamvere@khk.ee>
2018-01-15 09:02:57 +0200

    loome reaalse objekti test väärtusega

---
+++ obj/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri.tamvere
+ * Date: 15.01.2018
+ * Time: 8:57
+ */
+require_once 'tekst.php';
+
+// loome reaalse objekti tekst-klassi abil
+
+$minuTekst = new tekst();
+
+// teostame test-vaade antud objektist
+
+echo '<pre>';
+print_r ($minuTekst);
+echo '</pre>';
